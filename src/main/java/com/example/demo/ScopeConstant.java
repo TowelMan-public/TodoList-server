260ad9b638e253a08db69716b5ce64e2a404a876
TowelMan-public <OdagiriTomoya@outlook.jp>
2021-03-17 20:04:14 +0900

    データベース回り(repository,entity,dto,ディレクトリとSQLを書いたxml,MyBatisのジェネレーター）の修正・訂正

---
+++ src/main/java/com/example/demo/ScopeConstant.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class ScopeConstant {
+	public static final int PRIVATE = 3;
+	public static final int PROTECTED = 2;
+	public static final int PUBLIC = 1;
+}
---
+++ src/main/java/com/example/demo/UserAuthorityInSpaceConstant.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class UserAuthorityInSpaceConstant {
+	public static final int MASTER = 1;
+	public static final int SUPER = 2;
+	public static final int USER = 3;
+}
