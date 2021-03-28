dac514458642d864f0127623cd52e57c0686e6a2
TowelMan-public <OdagiriTomoya@outlook.jp>
2021-03-28 23:29:34 +0900

    UserInSpaceLogicSharedServiceTestの実装完了・オールグリーン

--- src/test/java/com/example/demo/logic/UserInSpaceLogicSharedServiceTest.java
+++ src/test/java/com/example/demo/logic/UserInSpaceLogicSharedServiceTest.java
@@ -1,12 +1,63 @@
 package com.example.demo.logic;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.TestMapper;
 import com.example.demo.exception.UserAleadyJoinSpaceException;
 
-public class UserInSpaceLogicSharedServiceTest {
+@SpringBootTest
+class UserInSpaceLogicSharedServiceTest {
 	@Autowired
 	UserInSpaceLogicSharedService userInSpaceLogicSharedService;
+	@Autowired
+	TestMapper testMapper;
 	
-	//void verificationNotJoinedUserInSpace(int spaceId, int userId) throws UserAleadyJoinSpaceException
+	private int userIdInSpace;
+	private int userIdInNoSpace;
+	private int spaceId;
+	
+	public UserInSpaceLogicSharedServiceTest(){
+		userIdInSpace = 1;
+		userIdInNoSpace = 2;
+		spaceId = 1;
+	}
+	
+	@BeforeEach
+	void befoer() {
+		testMapper.deleteAll("todo_content");
+		testMapper.deleteAll("todo_list_details");
+		testMapper.deleteAll("todo_list");
+		testMapper.deleteAll("todo_space_details");
+		testMapper.deleteAll("have_user_list");
+		testMapper.deleteAll("todo_space");
+		testMapper.deleteAll("users");
+		
+		//ユーザーデータセット
+		testMapper.insert("users", "1,'a','',1");//userIdInSpace
+		testMapper.insert("users", "2,'b','',1");//userIdInNoSpace
+		
+		//スペース作成
+		testMapper.insert("todo_space", "1");//spaceId
+		
+		//spaceIdにユーザー追加
+		testMapper.insert("have_user_list", "1,1,3");
+		
+	}
+	
+	@Test
+	@Transactional
+	void verificationNotJoinedUserInSpaceTest() {
+		//加入しているユーザー
+		Assertions.assertDoesNotThrow(
+				() -> userInSpaceLogicSharedService.verificationNotJoinedUserInSpace(spaceId,userIdInSpace));
+		
+		//加入していないユーザー
+		Assertions.assertThrows(UserAleadyJoinSpaceException.class,
+				() -> userInSpaceLogicSharedService.verificationNotJoinedUserInSpace(spaceId,userIdInNoSpace));
+	}
 }
